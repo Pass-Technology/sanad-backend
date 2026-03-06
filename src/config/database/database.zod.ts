37760e405e70a5d0f3675d26f5e625c9e3948b76
mostafa-pass <mostafa.elsaeed@trypass.ai>
2026-03-06 04:26:06 +0200

    work done: ssl:true

--- src/config/database/database.zod.ts
+++ src/config/database/database.zod.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 
 export const databaseEnvSchema = z.object({
-    DATABASE_URL: z.string().url().optional(),
-    DATABASE_HOST: z.string().default('localhost'),
-    DATABASE_PORT: z.coerce.number().int().positive().default(5432),
-    DATABASE_USER: z.string().default('postgres'),
-    DATABASE_PASSWORD: z.string(),
-    DATABASE_NAME: z.string().default('DB'),
+    DATABASE_URL: z.string().url(),
+    // DATABASE_HOST: z.string().default('localhost'),
+    // DATABASE_PORT: z.coerce.number().int().positive().default(5432),
+    // DATABASE_USER: z.string().default('postgres'),
+    // DATABASE_PASSWORD: z.string(),
+    // DATABASE_NAME: z.string().default('DB'),
 });
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,19 +6,19 @@ config();
 export default new DataSource({
     type: 'postgres',
     url: process.env.DATABASE_URL,
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    host: process.env.DATABASE_HOST,
-    port: Number(process.env.DATABASE_PORT),
+    // database: process.env.DATABASE_NAME,
+    // username: process.env.DATABASE_USERNAME,
+    // password: process.env.DATABASE_PASSWORD,
+    // host: process.env.DATABASE_HOST,
+    // port: Number(process.env.DATABASE_PORT),
     synchronize: false,
     logging: true,
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     migrations: [__dirname + '/migrations/*{.ts,.js}'],
-    ssl: false,
-    // extra: {
-    //     ssl: {
-    //         rejectUnauthorized: false,
-    //     },
-    // },
+    ssl: true,
+    extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
 });
