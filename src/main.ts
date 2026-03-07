f4fd8a75f5b20b32d6bba9915a6b4218ee8e74cb
mostafa-pass <mostafa.elsaeed@trypass.ai>
2026-03-07 22:18:33 +0200

    Work Done: 	hotfix: 		- enable the cors for all endpoints

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,12 @@ import { AppConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type,Accept,Authorization,X-Requested-With',
+  });
 
   app.setGlobalPrefix('api');
 
