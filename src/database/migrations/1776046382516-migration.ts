6f82e63bc17154214f7731e4ad03e2631556f214
mostafa-pass <mostafa.elsaeed@trypass.ai>
2026-04-16 14:19:50 +0200

    Work done: resolve migration error

--- src/database/migrations/1776046382516-migration.ts
+++ src/database/migrations/1776046382516-migration.ts
@@ -4,7 +4,7 @@ export class Migration1776046382516 implements MigrationInterface {
     name = 'Migration1776046382516'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "branches" ADD "is_available" boolean NOT NULL DEFAULT true`);
+        await queryRunner.query(`ALTER TABLE "branches" ADD COLUMN IF NOT EXISTS "is_available" boolean NOT NULL DEFAULT true`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
