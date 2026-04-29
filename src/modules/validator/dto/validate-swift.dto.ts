6308c772b3b5b8bbd2bb27a084782a8ac83baf05
mostafa-pass <mostafa.elsaeed@trypass.ai>
2026-04-30 02:19:27 +0300

    work done: chanage the source of bic file

--- src/modules/validator/dto/validate-swift.dto.ts
+++ src/modules/validator/dto/validate-swift.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
-import { IsValidBIC } from "../decorators/is-valid-BIC.validator";
+import { IsValidBIC } from "../decorators/is-valid-bic.validator";
 
 export class ValidateSwiftDto {
     @ApiProperty({ example: 'ADCBAEAA', description: 'SWIFT code to validate' })
--- src/modules/validator/validator.service.ts
+++ src/modules/validator/validator.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { electronicFormatIBAN } from 'ibantools';
 import { ValidateIbanDto } from './dto/validate-iban.dto';
 import { ValidateSwiftDto } from './dto/validate-swift.dto';
 
