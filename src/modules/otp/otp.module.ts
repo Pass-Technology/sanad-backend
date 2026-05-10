1ef9b313adcd3edb92ff523fa8cf4413380e97b8
mostafa-pass <mostafa.elsaeed@trypass.ai>
2026-05-10 18:51:49 +0300

    work done: resend endpoint in swagger

--- src/modules/otp/otp.module.ts
+++ src/modules/otp/otp.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OtpService } from './otp.service';
 import { OtpRepository } from './otp.repository';
 import { OtpEntity } from './entities/otp.entity';
+import { OtpController } from './otp.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([OtpEntity]),
   ],
+  controllers: [OtpController],
   providers: [OtpService, OtpRepository],
   exports: [OtpService, OtpRepository],
 })
